cec12ef8cb8d93043b6bbeceb045a45848ed7b87
Wayne Collier <ke4tri@gmail.com>
2019-08-21 06:57:19 -0500

    using map list on person state to render in jsx

--- src/App.js
+++ src/App.js
@@ -13,16 +13,22 @@ class App extends Component {
     showPersons: false
   };
 
-  switchNameHandler = newName => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: "Manu", age: 29 },
-        { name: "Stephanie", age: 27 }
-      ]
-    });
+  // switchNameHandler = newName => {
+  //   // console.log('Was clicked!');
+  //   // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+  //   this.setState({
+  //     persons: [
+  //       { name: newName, age: 28 },
+  //       { name: "Manu", age: 29 },
+  //       { name: "Stephanie", age: 27 }
+  //     ]
+  //   });
+  // };
+
+  deletePersonHandler = personIndex => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
   };
 
   nameChangedHandler = event => {
@@ -49,12 +55,22 @@ class App extends Component {
       cursor: "pointer"
     };
 
-      let persons = null;
+    let persons = null;
 
-      if (this.state.showPersons) {
-        persons = (
-          <div>
-          <Person
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                // click={this.deletePersonHandler}
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+              />
+            );
+          })}
+          {/* <Person
             name={this.state.persons[0].name}
             age={this.state.persons[0].age}
           />
@@ -69,11 +85,10 @@ class App extends Component {
           <Person
             name={this.state.persons[2].name}
             age={this.state.persons[2].age}
-          />
+          /> */}
         </div>
-        );
-      }
-
+      );
+    }
 
     return (
       <div className="App">
